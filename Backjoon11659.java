cc84848680e99b90597692423a2484e867b8b4e7
Hayden721 <hayden00721@gmail.com>
2025-03-05 21:50:49 +0900

    first commit

---
+++ Backjoon11659.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Backjoon11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int dataNo = Integer.parseInt(st.nextToken()); // 입력받을 데이터 개수
+        int quizNo = Integer.parseInt(st.nextToken()); // 질문 개수
+        long[] S = new long[dataNo + 1]; // 합 배열 선언
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=1; i<=dataNo; i++) { // 합 배열
+            S[i] = S[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        for(int q=0; q < quizNo; q++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            System.out.println(S[j] - S[i-1]);
+        }
+
+
+    }
+}
---
+++ Baekjoon10798.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+// 백준 10798
+public class Baekjoon10798 {
+
+    public static void main(String[] args) throws IOException {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[][] ch = new char[5][15];
+        int max = 0; // 입력한 문자의 최대 길이
+
+        for(int i=0; i<ch.length; i++) {
+            String str = br.readLine();
+
+            for(int j = 0; j < str.length(); j++) {
+                ch[i][j] = str.charAt(j);
+            }
+        }
+
+        for(int i=0; i<15; i++) {
+            for(int j=0; j<5; j++) {
+                if(ch[j][i] != '\u0000') {
+                    System.out.print(ch[j][i]);
+                }
+            }
+        }
+    }
+    
+}
